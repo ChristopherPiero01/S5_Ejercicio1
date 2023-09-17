db1858b36e6f6be7133003a68b21ad733105712b
USUARIO <USUARIO@DESKTOP-CVJUUEK>
2023-09-17 12:35:45 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp40/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp40
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese dos números: ");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int producto = 0;
+            for (int i = 1; i <= num2; i++) 
+            {
+                producto = producto + num1;
+            }
+            Console.WriteLine("El producto es: "+ producto);
+            Console.ReadKey();
+        }
+    }
+}
